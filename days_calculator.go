d502beb0ec4a48a224e1e695c33ae584240682ee
pan-asovsky <panosovski@gmail.com>
2023-01-19 22:11:40 +0000

    minor style fix + gitignore

--- days_calculator.go
+++ days_calculator.go
@@ -12,46 +12,48 @@ import (
 func main() {
 
 	router := gin.Default()
-	router.GET("/when/:year", func(context *gin.Context) {
-
-		year := context.Param("year")
-		yearAsInt, err := strconv.Atoi(year)
-
-		if err != nil {
-			log.Println("Error converting string to int: ", err)
-		}
-
-		// 2006 используется как формат даты для функции time.Parse().
-		// При изменении на любой другой год, формат не будет соответствовать и будет выброшена ошибка.
-		dateString := "01/01/" + year
-		dateLayout := "01/01/2006"
-		parsedDate, err := time.Parse(dateLayout, dateString)
-
-		if err != nil {
-			log.Println("Error parse date: ", err)
-			context.String(http.StatusBadRequest, "Некорректная дата")
-			return
-		}
-
-		currentTime := time.Now()
-		currentMonth := currentTime.Month()
-		currentYear := currentTime.Year()
-
-		daysUntil := parsedDate.Sub(currentTime).Hours() / 24
-		daysPassed := currentTime.Sub(parsedDate).Hours() / 24
-
-		if currentYear == yearAsInt && currentMonth == parsedDate.Month() && currentTime.Day() == parsedDate.Day() {
-			context.String(http.StatusOK, "Сегодня 1 января")
-		} else if currentYear < yearAsInt {
-			context.String(http.StatusOK, "До 1 января "+year+" года осталось "+strconv.Itoa(int(daysUntil))+" дней")
-		} else {
-			context.String(http.StatusOK, "С 1 января "+year+" года прошло "+strconv.Itoa(int(math.Abs(daysPassed)))+" дней")
-		}
-	})
+	router.GET("/when/:year", whenYearRouteHandler)
 
 	err := router.Run()
 	if err != nil {
 		log.Fatalln("Fatal error at router.Run(): ", err)
+	}
+}
+
+func whenYearRouteHandler(context *gin.Context) {
+
+	year := context.Param("year")
+	yearAsInt, err := strconv.Atoi(year)
+
+	if err != nil {
+		log.Println("Error converting string to int: ", err)
+	}
+
+	// 2006 используется как формат даты для функции time.Parse().
+	// При изменении на другой год, формат перестаёт соответствовать и выбрасывается ошибка.
+	dateString := "01/01/" + year
+	dateLayout := "01/01/2006"
+	parsedDate, err := time.Parse(dateLayout, dateString)
+
+	if err != nil {
+		log.Println("Error parse date: ", err)
+		context.String(http.StatusBadRequest, "Некорректная дата")
+		return
+	}
+
+	currentTime := time.Now()
+	currentMonth := currentTime.Month()
+	currentYear := currentTime.Year()
+
+	daysUntil := parsedDate.Sub(currentTime).Hours() / 24
+	daysPassed := currentTime.Sub(parsedDate).Hours() / 24
+
+	if currentYear == yearAsInt && currentMonth == parsedDate.Month() && currentTime.Day() == parsedDate.Day() {
+		context.String(http.StatusOK, "Сегодня 1 января")
+	} else if currentYear < yearAsInt {
+		context.String(http.StatusOK, "До 1 января "+year+" года осталось "+strconv.Itoa(int(daysUntil))+" дней")
+	} else {
+		context.String(http.StatusOK, "С 1 января "+year+" года прошло "+strconv.Itoa(int(math.Abs(daysPassed)))+" дней")
 	}
 
 }
