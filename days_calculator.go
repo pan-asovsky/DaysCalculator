beba0135b0c08167bb751c702a3643d2066ed3d6
pan-asovsky <panosovski@gmail.com>
2023-01-20 11:39:48 +0000

    final

--- days_calculator.go
+++ days_calculator.go
@@ -39,8 +39,6 @@ func whenYearRouteHandler(context *gin.Context) {
 		log.Println("Error converting string to int: ", err)
 	}
 
-	// 2006 используется как формат даты для функции time.Parse().
-	// При изменении на другой год, формат перестаёт соответствовать и выбрасывается ошибка.
 	dateString := "01/01/" + year
 	dateLayout := "01/01/2006"
 	parsedDate, err := time.Parse(dateLayout, dateString)
@@ -52,18 +50,21 @@ func whenYearRouteHandler(context *gin.Context) {
 	}
 
 	currentTime := time.Now()
-	currentMonth := currentTime.Month()
 	currentYear := currentTime.Year()
 
 	daysUntil := parsedDate.Sub(currentTime).Hours() / 24
 	daysPassed := currentTime.Sub(parsedDate).Hours() / 24
 
-	if currentYear == yearAsInt && currentMonth == parsedDate.Month() && currentTime.Day() == parsedDate.Day() {
+	if currentYear == yearAsInt && currentTime.Month() == parsedDate.Month() && currentTime.Day() == parsedDate.Day() {
 		context.String(http.StatusOK, "Today 1st january")
 	} else if currentYear < yearAsInt {
-		context.String(http.StatusOK, "Days left: "+strconv.Itoa(int(daysUntil)))
+		context.String(http.StatusOK, "Days left: "+IntToString(daysUntil))
 	} else {
-		context.String(http.StatusOK, "Days gone: "+strconv.Itoa(int(math.Abs(daysPassed))))
+		context.String(http.StatusOK, "Days gone: "+IntToString(math.Abs(daysPassed)))
 	}
 
+}
+
+func IntToString(num float64) string {
+	return strconv.Itoa(int(num))
 }
