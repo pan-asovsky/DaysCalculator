c749252a9d71665a7729e12ac097b3f4d91a6fee
pan-asovsky <panosovski@gmail.com>
2023-01-20 18:06:48 +0000

    .

--- tests/days_calculator_test.go
+++ tests/days_calculator_test.go
@@ -11,31 +11,6 @@ import (
 	"time"
 )
 
-func TestXPingHeaderMiddleware(t *testing.T) {
-
-	gin.SetMode(gin.TestMode)
-
-	router := gin.New()
-	router.Use(server.XPingHeaderMiddleware())
-
-	req, _ := http.NewRequest("GET", "/test", nil)
-	req.Header.Add("X-PING", "ping")
-	resp := httptest.NewRecorder()
-	router.ServeHTTP(resp, req)
-
-	if resp.Header().Get("X-PONG") != "pong" {
-		t.Error("The expected X-PONG header should be set to 'pong'")
-	}
-
-	req, _ = http.NewRequest("GET", "/test", nil)
-	resp = httptest.NewRecorder()
-	router.ServeHTTP(resp, req)
-
-	if resp.Header().Get("X-PONG") == "pong" {
-		t.Error("The expected X-PONG header should not be set")
-	}
-}
-
 func TestWhenYearRouteHandler(t *testing.T) {
 
 	gin.SetMode(gin.TestMode)
